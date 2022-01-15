4ca62ad56a5523710a91895abad23220ed6019c0
Joel Dennison <joel.dennison780@gmail.com>
2022-01-15 13:47:42 -0500

    flake8 formatting

--- src/qrzpal.py
+++ src/qrzpal.py
@@ -4,35 +4,35 @@ from dotenv import dotenv_values
 import math
 from datetime import datetime
 
-BASE_URL = 'https://logbook.qrz.com/api'
+BASE_URL = "https://logbook.qrz.com/api"
 
 
-def parse_freq(f):
+def parse_freq(f): # noqa C901
     fbase = int(math.floor(f))
     if fbase == 1:
-        return '160m'
-    elif fbase == 3 or f == 4:
-        return '80m'
+        return "160m"
+    elif fbase == 3 or fbase == 4:
+        return "80m"
     elif fbase == 5:
-        return '60m'
+        return "60m"
     elif fbase == 7:
-        return '40m'
+        return "40m"
     elif fbase == 10:
-        return '30m'
+        return "30m"
     elif fbase == 14:
-        return '20m'
+        return "20m"
     elif fbase == 18:
-        return '17m'
+        return "17m"
     elif fbase == 21:
-        return '15m'
+        return "15m"
     elif fbase == 24:
-        return '12m'
+        return "12m"
     elif fbase == 28 or fbase == 29:
-        return '10m'
+        return "10m"
     elif fbase >= 50 and fbase <= 54:
-        return '6m'
+        return "6m"
     elif fbase >= 144 and fbase <= 148:
-        return '2m'
+        return "2m"
     else:
         return None
 
@@ -42,10 +42,9 @@ def build_req_adif_str(data):
     if data.band is None:
         return None
 
-    d = datetime.strptime(f"{data.date_end} {data.time_end}",
-                          '%Y-%m-%d %H:%M:%S')
-    qso_date = d.strftime('%Y%m%d')
-    qso_time = d.strftime('%H%M')
+    d = datetime.strptime(f"{data.date_end} {data.time_end}", "%Y-%m-%d %H:%M:%S")
+    qso_date = d.strftime("%Y%m%d")
+    qso_time = d.strftime("%H%M")
 
     return f"<band:{len(data.band)}>{data.band}\
         <mode:{len(data.qso_mode)}>{data.qso_mode}\
@@ -55,27 +54,40 @@ def build_req_adif_str(data):
         <time_on:{len(qso_time)}>{qso_time}\
         <tx_pwr:{len(str(data.power))}>{str(data.power)}\
         <freq:{len(str(data.frequency))}>{str(data.frequency)}\
-        <eor>".replace(' ', '')
+        <eor>".replace(
+        " ", ""
+    )
 
 
-if __name__ == '__main__':
-    env = dotenv_values('.env')
+if __name__ == "__main__":
+    env = dotenv_values(".env")
 
     logs = pd.read_csv(
-        env['LOG_PATH'],
-        names=['date_start', 'time_start', 'date_end', 'time_end', 'call',
-               'square', 'frequency', 'qso_mode', 'sent', 'received',
-               'power', 'unk0', 'unk1', 'unk2'])
+        env["LOG_PATH"],
+        names=[
+            "date_start",
+            "time_start",
+            "date_end",
+            "time_end",
+            "call",
+            "square",
+            "frequency",
+            "qso_mode",
+            "sent",
+            "received",
+            "power",
+            "unk0",
+            "unk1",
+            "unk2",
+        ],
+    )
 
-    logs['band'] = logs['frequency'].apply(parse_freq)
-    logs['request'] = logs.apply(build_req_adif_str, axis=1)
+    logs["band"] = logs["frequency"].apply(parse_freq)
+    logs["request"] = logs.apply(build_req_adif_str, axis=1)
 
     for _, r in logs.iterrows():
         resp = requests.post(
             BASE_URL,
-            data={
-                'KEY': env['USR_KEY'],
-                'ACTION': 'INSERT',
-                'ADIF': r.request
-            })
+            data={"KEY": env["USR_KEY"], "ACTION": "INSERT", "ADIF": r.request},
+        )
         print(resp.text)
